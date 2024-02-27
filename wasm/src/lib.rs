27c99a62838fc236ec6a0dc964dd54551bef0042
Kyohei Uto <h27.kubb@gmail.com>
2024-02-28 00:19:07 +0900

    Add util module and update gen and vis functions

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,9 @@
 use wasm_bindgen::prelude::*;
+mod util;
 
 #[wasm_bindgen]
 pub fn gen(seed: i32) -> String {
-    "".to_string()
+    util::gen(seed as u64).to_string()
 }
 
 #[wasm_bindgen(getter_with_clone)]
@@ -14,14 +15,23 @@ pub struct Ret {
 
 #[wasm_bindgen]
 pub fn vis(_input: String, _output: String, turn: usize) -> Ret {
+    let input = util::parse_input(&_input);
+    // _outputが空文字の場合"0"にする
+    let _output = if _output.is_empty() { "0" } else { &_output };
+    let output = util::parse_output(&input, &_output);
+    let (score, err, svg) = match output {
+        Ok(output) => util::vis(&input, &output, turn),
+        Err(err) => (0, err, "".to_string()),
+    };
     Ret {
-        score: 0,
-        err: "".to_string(),
-        svg: "".to_string(),
+        score,
+        err,
+        svg,
     }
 }
 
 #[wasm_bindgen]
 pub fn get_max_turn(_input: String, _output: String) -> usize {
-    0
+    let input = util::parse_input(&_input);
+    input.T
 }
