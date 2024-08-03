cd6a3eda7325743a5574edfd04b2f6c970c85150
Kyohei Uto <h27.kubb@gmail.com>
2024-08-03 15:14:53 +0900

    add

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,11 @@
 use wasm_bindgen::prelude::*;
+mod util;
+
+const PROBLEM_ID: char = 'C';
 
 #[wasm_bindgen]
 pub fn gen(seed: i32) -> String {
-    "".to_string()
+    util::gen(seed as u64, PROBLEM_ID).to_string()
 }
 
 #[wasm_bindgen(getter_with_clone)]
@@ -14,14 +17,19 @@ pub struct Ret {
 
 #[wasm_bindgen]
 pub fn vis(_input: String, _output: String, turn: usize) -> Ret {
+    let input = util::parse_input(&_input);
+    let output = util::parse_output(&input, &_output).unwrap();
+    let (score, err, svg) = util::vis(&input, &output, turn);
     Ret {
-        score: 0,
-        err: "".to_string(),
-        svg: "".to_string(),
+        score,
+        err: err.to_string(),
+        svg: svg.to_string(),
     }
 }
 
 #[wasm_bindgen]
 pub fn get_max_turn(_input: String, _output: String) -> usize {
-    0
+    let input = util::parse_input(&_input);
+    let output = util::parse_output(&input, &_output).unwrap();
+    output.out.len() - 1
 }
